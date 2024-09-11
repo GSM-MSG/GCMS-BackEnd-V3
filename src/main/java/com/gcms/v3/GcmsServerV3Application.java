e26a4a7ce3f0ae757071bc05c723c3393f481612
ta2ye0n <tyeon42051@naver.com>
2024-09-11 11:20:25 +0900

    init

---
+++ src/main/java/com/gcms/v3/GcmsServerV3Application.java
@@ -0,0 +1,13 @@
+package com.gcms.v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcmsServerV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcmsServerV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcms/v3/GcmsServerV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcms.v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcmsServerV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
