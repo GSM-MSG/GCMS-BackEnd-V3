f55a70a2e014cbe3aef3e20935daafd844a8ca17
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-27 20:39:06 +0900

    add :: Applicant 엔티티 추가

---
+++ src/main/java/com/gcms/v3/domain/applicant/domain/entity/Applicant.java
@@ -0,0 +1,32 @@
+package com.gcms.v3.domain.applicant.domain.entity;
+
+import com.gcms.v3.domain.club.domain.entity.Club;
+import com.gcms.v3.domain.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Applicant {
+
+    @Id
+    @GeneratedValue(generator = "ulidGenerator")
+    @GenericGenerator(name = "ulidGenerator", strategy = "com.gcms.v3.global.common.ulid.ULIDGenerator")
+    @Column(nullable = false, unique = true)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
