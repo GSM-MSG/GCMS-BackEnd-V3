dba7be744aefaf2c70f8002bde3779eb2308a13d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-27 20:40:43 +0900

    add :: Notice 엔티티 추가

---
+++ src/main/java/com/gcms/v3/domain/notice/domain/Notice.java
@@ -0,0 +1,30 @@
+package com.gcms.v3.domain.notice.domain;
+
+import com.gcms.v3.domain.user.domain.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class Notice {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
