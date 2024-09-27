390e6937578846d60b2a3b25fc2092840dd8973d
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-09-27 20:41:14 +0900

    add :: UserMajor 엔티티 추가

---
+++ src/main/java/com/gcms/v3/domain/user/domain/entity/UserMajor.java
@@ -0,0 +1,31 @@
+package com.gcms.v3.domain.user.domain.entity;
+
+import com.gcms.v3.domain.user.domain.enums.Major;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class UserMajor {
+
+    @Id
+    @GeneratedValue(generator = "ulidGenerator")
+    @GenericGenerator(name = "ulidGenerator", strategy = "com.gcms.v3.global.common.ulid.ULIDGenerator")
+    @Column(nullable = false, unique = true)
+    private String id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Major major;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
