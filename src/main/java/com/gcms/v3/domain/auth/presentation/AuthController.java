f71bc22dcc09f85ec8c8f5c57813ee5d5d5ab960
ta2ye0n <tyeon42051@naver.com>
2024-10-08 11:16:03 +0900

    add :: AuthController

---
+++ src/main/java/com/gcms/v3/domain/auth/presentation/AuthController.java
@@ -0,0 +1,22 @@
+package com.gcms.v3.domain.auth.presentation;
+
+import com.gcms.v3.domain.auth.presentation.data.request.SignInRequestDto;
+import com.gcms.v3.domain.auth.presentation.data.response.TokenInfoResponseDto;
+import com.gcms.v3.domain.auth.service.SignInService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v3/auth")
+public class AuthController {
+
+    private final SignInService signInService;
+
+    @PostMapping
+    public ResponseEntity<TokenInfoResponseDto> test(@RequestBody SignInRequestDto signInRequestDto) {
+        TokenInfoResponseDto res = signInService.execute(signInRequestDto);
+        return ResponseEntity.ok(res);
+    }
+}
---
+++ src/main/java/com/gcms/v3/domain/auth/presentation/data/request/SignInRequestDto.java
@@ -0,0 +1,8 @@
+package com.gcms.v3.domain.auth.presentation.data.request;
+
+import lombok.Getter;
+
+@Getter
+public class SignInRequestDto {
+    private String code;
+}
