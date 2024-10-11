e06e043395a49ab0456aa2d82c9a2787d0bf72f8
ta2ye0n <tyeon42051@naver.com>
2024-10-11 11:09:27 +0900

    update :: record 변경

--- src/main/java/com/gcms/v3/domain/auth/presentation/data/response/TokenInfoResponseDto.java
+++ src/main/java/com/gcms/v3/domain/auth/presentation/data/response/TokenInfoResponseDto.java
@@ -1,15 +1,14 @@
 package com.gcms.v3.domain.auth.presentation.data.response;
 
 import lombok.Builder;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
 @Builder
-public class TokenInfoResponseDto {
-    private String accessToken;
-    private String refreshToken;
-    private LocalDateTime accessTokenExpiresIn;
-    private LocalDateTime refreshTokenExpiresIn;
+public record TokenInfoResponseDto(
+        String accessToken,
+        String refreshToken,
+        LocalDateTime accessTokenExpiresIn,
+        LocalDateTime refreshTokenExpiresIn
+) {
 }
