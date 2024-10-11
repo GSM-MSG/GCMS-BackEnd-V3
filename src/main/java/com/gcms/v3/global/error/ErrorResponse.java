292e0055e42491eccee58946fcbf66cf2a91b553
ta2ye0n <tyeon42051@naver.com>
2024-10-11 12:03:43 +0900

    update :: record 변경

--- src/main/java/com/gcms/v3/global/error/ErrorResponse.java
+++ src/main/java/com/gcms/v3/global/error/ErrorResponse.java
@@ -1,18 +1,14 @@
 package com.gcms.v3.global.error;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-@Getter
 @Builder
-@AllArgsConstructor
-public class ErrorResponse {
-    private HttpStatus status;
-    private String message;
-
+public record ErrorResponse(
+        HttpStatus status,
+        String message
+) {
     public static ResponseEntity<ErrorResponse> toResponseEntity(ErrorCode e){
         return ResponseEntity
                 .status(e.getHttpStatus())
