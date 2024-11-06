4350cd38194107dc45621a9772ad591694413095
ta2ye0n <tyeon42051@naver.com>
2024-11-06 11:18:50 +0900

    refactor :: @Transactional

--- src/main/java/com/gcms/v3/global/redis/RedisConfig.java
+++ src/main/java/com/gcms/v3/global/redis/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.gcms.v3.global.redis;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,11 +20,13 @@ public class RedisConfig {
     }
 
     @Bean
+    @Transactional
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setValueSerializer(new StringRedisSerializer());
         template.setHashValueSerializer(new StringRedisSerializer());
         template.setConnectionFactory(redisConnectionFactory());
+        template.setEnableTransactionSupport(true);
         return template;
     }
 }
